24aec2758b6b24d02f7f46fb9dfceb2e5c5e881e
tdoelp <tdoelp@users.noreply.github.com>
2015-04-05 14:38:21 -0400

    Update DBConnect.java

--- DBConnect.java
+++ DBConnect.java
@@ -1,3 +1,4 @@
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -10,50 +11,43 @@ import java.sql.Statement;
  * @author Tom
  */
 public class DBConnect {
-    
+
     static Connection con = null;
     static Statement s = null;
     static ResultSet result;
-    
-    public DBConnect(){
+
+    public DBConnect() {
         String username = "amm216";
         String pass = "Archie1994";
-                
-               try{
-                     Class.forName ("oracle.jdbc.driver.OracleDriver");
-        con = DriverManager.getConnection
-        ("jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241","" + username + "",
-         "" + pass + "");
-            s=con.createStatement();
-                
-   
-        }
-        catch(java.lang.ClassNotFoundException e2){
-                System.out.println("Missing necessary driver..");  
-        }
-        catch(SQLException e){
-                System.out.println("Invalid username and password..");
-        }
-        catch(Exception e){
+
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            con = DriverManager.getConnection("jdbc:oracle:thin:@edgar1.cse.lehigh.edu:1521:cse241", "" + username + "",
+                    "" + pass + "");
+            s = con.createStatement();
+
+        } catch (java.lang.ClassNotFoundException e2) {
+            System.out.println("Missing necessary driver..");
+        } catch (SQLException e) {
+            System.out.println("Invalid username and password..");
+        } catch (Exception e) {
             System.out.println("error");
         }
     }
-    
-    public void query(String q){
-        try{
+
+    public void query(String q) throws Exception{
+        try {
             result = s.executeQuery(q);
             ResultSetMetaData rsmd = result.getMetaData();
-        }
-        catch(Exception e){
-            System.out.println("error");
+        } catch (Exception e) {
+            throw new Exception();
         }
     }
-    
-    public String getString(String s){
-        try{
+
+    public String getString(String s) {
+        try {
             return result.getString(s);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println("error");
             return "seqmentation fault";
         }
